24dae4f321a27d7902d781518f48c7c11977e65a
Moreau <guen94@hotmail.fr>
2021-12-08 13:44:15 +0100

    transition entre intro et one

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,26 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class MainCamera : Singleton<MainCamera>
 {
-    private Transform CameraTransform;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void SceneController (Collider state) {
-        if (state.name.CompareTo("SceneOne") == 0 ) {
-            CameraTransform.DOMoveZ(5, 2);
-            CameraTransform.DOMoveY(3, 2);
-        }
-    }
 }
